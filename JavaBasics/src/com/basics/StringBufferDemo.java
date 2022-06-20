d3b62b6f50824082443a655f08d84c6e9db4b83a
Bheemaraya <Bheemaraya@DESKTOP-UT1LUVE>
2022-06-20 11:31:06 +0530

    StringBuffer class

---
+++ JavaBasics/src/com/basics/StringBufferDemo.java
@@ -0,0 +1,28 @@
+package com.basics;
+
+public class StringBufferDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer("C++");
+		
+		System.out.println("Length "+sb.length());
+		System.out.println("Capacity"+sb.capacity());
+		System.out.println(sb.charAt(0));
+		sb.setCharAt(0, 'j');
+		System.out.println(sb);
+		
+		StringBuffer sb1 = new StringBuffer("India");
+		System.out.println(sb1);
+		System.out.println(sb1.reverse());
+		
+
+		StringBuffer sb2 = new StringBuffer("Java Programming ");
+		System.out.println(sb2);
+		sb2.replace(0, 4, "C");
+		System.out.println(sb2);
+		
+
+	}
+
+}
