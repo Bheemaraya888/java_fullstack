a22442584c9ff629b385a547d4c42d39c68c1171
Bheemaraya <Bheemaraya@DESKTOP-UT1LUVE>
2022-07-17 17:20:01 +0530

    Exception Handling

---
+++ JavaBasics/src/com/exceptionhandling/ExceptionHandlingDemo1.java
@@ -0,0 +1,22 @@
+package com.exceptionhandling;
+
+/*
+ * @author Bheemaraya
+ */
+
+
+public class ExceptionHandlingDemo1 {
+
+	public static void main(String[] args) {
+		try {
+		int data=100/0;
+		}
+		catch(ArithmeticException ae)
+		{
+			ae.printStackTrace();
+		}
+		System.out.println("Exception Handling Demo");
+
+	}
+
+}
---
+++ JavaBasics/src/com/exceptionhandling/ExceptionHandlingDemo4.java
@@ -0,0 +1,32 @@
+package com.exceptionhandling;
+
+import java.io.IOException;
+/*
+ * @author Bheemaraya
+ */
+
+public class ExceptionHandlingDemo4 {
+	void m()throws IOException
+	{
+	throw new IOException("device error");//checked exception
+	}
+	void n()throws IOException
+	{
+	m();
+	}
+	void p()
+	{
+	try
+	{
+	n();
+	}
+	catch(Exception e){System.out.println("exception handled");}
+	}
+	public static void main(String args[])
+	{
+  ExceptionHandlingDemo4 obj=new ExceptionHandlingDemo4();
+	obj.p();
+	System.out.println("normal flow...");
+	}
+
+}
